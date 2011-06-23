bf7800ffcb1942a0e0c7bfed4d079153805caeb4
xh176233756 <xh176233756@de93be47-ede6-e22e-746d-4a4661e0fcec>
2011-06-23 15:01:21 +0000

    UVa live题面加入pdf链接

--- judge/src/judge/spider/UVALiveSpider.java
+++ judge/src/judge/spider/UVALiveSpider.java
@@ -36,6 +36,8 @@ public class UVALiveSpider extends Spider {
 
 		//抓标题、时限
 		int category = Integer.parseInt(problem.getOriginProb()) / 100;
+		String pdfLink = "<span style='float:right'><a target='_blank' href='http://livearchive.onlinejudge.org/external/" + category + "/" + problem.getOriginProb() + ".pdf'><img width='100' height='26' border='0' title='Download as PDF' alt='Download as PDF' src='http://livearchive.onlinejudge.org/components/com_onlinejudge/images/button_pdf.png'></a></span><div style='clear:both'></div>";
+		description.setDescription(pdfLink);
 		getMethod = new GetMethod("http://livearchive.onlinejudge.org/external/" + category + "/" + problem.getOriginProb() + ".html");
 		getMethod.getParams().setParameter(HttpMethodParams.RETRY_HANDLER, new DefaultHttpMethodRetryHandler());
 		try {
@@ -46,7 +48,7 @@ public class UVALiveSpider extends Spider {
 			}
 			byte[] responseBody = getMethod.getResponseBody();
 			tLine = new String(responseBody, "UTF-8");
-			tLine = tLine.replaceAll("(?i)(src=\"?)(?!\"*http)", "$1http://livearchive.onlinejudge.org/external/" + category + "/");
+			tLine = pdfLink + tLine.replaceAll("(?i)(src=\"?)(?!\"*http)", "$1http://livearchive.onlinejudge.org/external/" + category + "/");
 			description.setDescription(tLine);
 		} catch (Exception e) {
 			getMethod.releaseConnection();
