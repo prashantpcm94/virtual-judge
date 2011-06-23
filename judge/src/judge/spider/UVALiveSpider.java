48024339610eddd844823657ff45bb3162701be5
xh176233756 <xh176233756@de93be47-ede6-e22e-746d-4a4661e0fcec>
2011-06-23 14:25:21 +0000

    修正UVa live无题面不能抓题的bug

--- judge/src/judge/spider/UVALiveSpider.java
+++ judge/src/judge/spider/UVALiveSpider.java
@@ -13,26 +13,9 @@ public class UVALiveSpider extends Spider {
 		if (!problem.getOriginProb().matches("\\d+")) {
 			throw new Exception();
 		}
-		int category = Integer.parseInt(problem.getOriginProb()) / 100;
-		GetMethod getMethod = new GetMethod("http://livearchive.onlinejudge.org/external/" + category + "/" + problem.getOriginProb() + ".html");
-		getMethod.getParams().setParameter(HttpMethodParams.RETRY_HANDLER, new DefaultHttpMethodRetryHandler());
-		try {
-			int statusCode = httpClient.executeMethod(getMethod);
-			if (statusCode != HttpStatus.SC_OK) {
-				System.err.println("Method failed: " + getMethod.getStatusLine());
-				throw new Exception();
-			}
-			byte[] responseBody = getMethod.getResponseBody();
-			tLine = new String(responseBody, "UTF-8");
-		} catch (Exception e) {
-			getMethod.releaseConnection();
-			throw new Exception();
-		}
-
-		tLine = tLine.replaceAll("(?i)(src=\"?)(?!\"*http)", "$1http://livearchive.onlinejudge.org/external/" + category + "/");
-		description.setDescription(tLine);
 		
-		getMethod = new GetMethod("http://livearchive.onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&page=show_problem&problem=" + (Integer.parseInt(problem.getOriginProb()) - 1999));
+		//抓描述
+		GetMethod getMethod = new GetMethod("http://livearchive.onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&page=show_problem&problem=" + (Integer.parseInt(problem.getOriginProb()) - 1999));
 		getMethod.getParams().setParameter(HttpMethodParams.RETRY_HANDLER, new DefaultHttpMethodRetryHandler());
 		try {
 			int statusCode = httpClient.executeMethod(getMethod);
@@ -46,11 +29,28 @@ public class UVALiveSpider extends Spider {
 			getMethod.releaseConnection();
 			throw new Exception();
 		}
-
 		problem.setTitle(regFind(tLine, "<h3>" + problem.getOriginProb() + " - ([\\s\\S]+?)</h3>"));
 		problem.setTimeLimit(Integer.parseInt(regFind(tLine, "Time limit: ([\\d\\.]+)").replaceAll("\\.", "")));
 		problem.setMemoryLimit(0);
 		problem.setUrl("http://livearchive.onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&page=show_problem&problem=" + (Integer.parseInt(problem.getOriginProb()) - 1999));
+
+		//抓标题、时限
+		int category = Integer.parseInt(problem.getOriginProb()) / 100;
+		getMethod = new GetMethod("http://livearchive.onlinejudge.org/external/" + category + "/" + problem.getOriginProb() + ".html");
+		getMethod.getParams().setParameter(HttpMethodParams.RETRY_HANDLER, new DefaultHttpMethodRetryHandler());
+		try {
+			int statusCode = httpClient.executeMethod(getMethod);
+			if (statusCode != HttpStatus.SC_OK) {
+				System.err.println("Method failed: " + getMethod.getStatusLine());
+				throw new Exception();
+			}
+			byte[] responseBody = getMethod.getResponseBody();
+			tLine = new String(responseBody, "UTF-8");
+			tLine = tLine.replaceAll("(?i)(src=\"?)(?!\"*http)", "$1http://livearchive.onlinejudge.org/external/" + category + "/");
+			description.setDescription(tLine);
+		} catch (Exception e) {
+			getMethod.releaseConnection();
+		}
 	}
 
 	@Override
