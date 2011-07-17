5956375c18f80d3e16ca1719219ebb88aa1f24e6
xh176233756 <xh176233756@de93be47-ede6-e22e-746d-4a4661e0fcec>
2011-07-17 01:39:36 +0000

    更改比赛列表默认点击排序方向

--- judge/WebRoot/javascript/listContest.js
+++ judge/WebRoot/javascript/listContest.js
@@ -17,56 +17,59 @@ $(document).ready(function() {
 		"bAutoWidth": false,
 		"bStateSave": true,
 		"aaSorting": [[ 2, "desc" ]],
+		"aoColumnDefs": [
+			{ "asSorting": [ "desc", "asc" ], "aTargets": [ "_all" ] }
+		],
 
 		"aoColumns": [
-		  			{ 
-		  				"sClass": "id"
-		  			},
-		  			{
-		  				"fnRender": function ( oObj ) {
-			  				return "<a href='contest/viewContest.action?cid=" + oObj.aData[0] + "'>" + oObj.aData[1] + "</a>";
-		  				},
-		  				"sClass": "title"
-		  			},
-{
-		  				"fnRender": function ( oObj ) {
-			  				return new Date(parseInt(oObj.aData[2])).format("yyyy-MM-dd hh:mm:ss");
-		  				},
-		  				"sClass": "date"
-		  			},
-		  			{ 
-		  				"bSortable": false,
-		  				"sClass": "time"
-		  			},
-		  			{
-		  				"bSortable": false,
-		  				"sClass": "center status"
-		  			},
-		  			{ 
-		  				"bSortable": false,
-		  				"sClass": "center type"
-		  			},
-		  			{ 
-		  				"fnRender": function ( oObj ) {
-		  					return "<a href='user/profile.action?uid=" + oObj.aData[7] + "'>" + oObj.aData[6] + "</a>";
-		  				},
-		  				"sClass": "center"
-		  			},
-		  			{
-		  				"bVisible": false
-		  			},
-		  			{ 
-		  				"fnRender": function ( oObj ) {
-		  					if (oObj.aData[8] == 1) {
-		  						return "<a href='contest/toEditContest.action?cid=" + oObj.aData[0] + "'><img height='15px' border='0' src='images/wrench.gif' /></a>&nbsp;<a href='javascript:void(0)' onclick='comfirmDeleteContest(\"" + oObj.aData[0] + "\")'><img height='15px' border='0' src='images/recycle.gif' /></a>";
-		  					} else return "";
-		  				},
-		  				"bUseRendered": false, 
-		  				"bSearchable": false,
-		  				"bSortable": false,
-		  				"sClass": "id icon"
-		  			}
-		  		],
+					{ 
+						"sClass": "id"
+					},
+					{
+						"fnRender": function ( oObj ) {
+							return "<a href='contest/viewContest.action?cid=" + oObj.aData[0] + "'>" + oObj.aData[1] + "</a>";
+						},
+						"sClass": "title"
+					},
+					{
+						"fnRender": function ( oObj ) {
+							return new Date(parseInt(oObj.aData[2])).format("yyyy-MM-dd hh:mm:ss");
+						},
+						"sClass": "date"
+					},
+					{ 
+						"bSortable": false,
+						"sClass": "time"
+					},
+					{
+						"bSortable": false,
+						"sClass": "center status"
+					},
+					{ 
+						"bSortable": false,
+						"sClass": "center type"
+					},
+					{ 
+						"fnRender": function ( oObj ) {
+							return "<a href='user/profile.action?uid=" + oObj.aData[7] + "'>" + oObj.aData[6] + "</a>";
+						},
+						"sClass": "center"
+					},
+					{
+						"bVisible": false
+					},
+					{ 
+						"fnRender": function ( oObj ) {
+							if (oObj.aData[8] == 1) {
+								return "<a href='contest/toEditContest.action?cid=" + oObj.aData[0] + "'><img height='15px' border='0' src='images/wrench.gif' /></a>&nbsp;<a href='javascript:void(0)' onclick='comfirmDeleteContest(\"" + oObj.aData[0] + "\")'><img height='15px' border='0' src='images/recycle.gif' /></a>";
+							} else return "";
+						},
+						"bUseRendered": false, 
+						"bSearchable": false,
+						"bSortable": false,
+						"sClass": "id icon"
+					}
+				],
 		"fnServerData": function ( sSource, aoData, fnCallback ) {
 			var s = $("[name='scheduled']").attr("checked");
 			var r = $("[name='running']").attr("checked");
@@ -86,9 +89,9 @@ $(document).ready(function() {
 			} );
 		},
 		"fnRowCallback": function( nRow, aData, iDisplayIndex, iDisplayIndexFull ) {
-		    $('td:eq(4)', nRow).addClass(aData[4]);
-		    $('td:eq(5)', nRow).addClass(aData[5]);
-		    nRow.className += " " + aData[4];
+			$('td:eq(4)', nRow).addClass(aData[4]);
+			$('td:eq(5)', nRow).addClass(aData[5]);
+			nRow.className += " " + aData[4];
 			return nRow;
 		},
 		"bJQueryUI": true,
